408fffb1d85889aa3950c6ab141b7b235c249b08
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-25 22:59:47 +0200

    feature: add javascript

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './template.html';
 import './assets/styles/index.scss';
+
+const checkbox = document.querySelector('.switch-mode__checkbox');
+
+const onChange = () => {
+  sessionStorage.setItem('checked-value', checkbox.checked);
+
+  if (checkbox.checked) {
+    document.body.style.backgroundColor = 'blue';
+  } else {
+    document.body.style.backgroundColor = 'white';
+  }
+};
+
+const savedValue = sessionStorage.getItem('checked-value');
+
+if (savedValue !== null) {
+  checkbox.checked = JSON.parse(savedValue);
+  onChange();
+}
+
+checkbox.addEventListener('change', onChange);
