d6ed5870a7de402d0537d6b9a88b710e9a631814
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-02-25 12:54:53 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './template.html';
+import './assets/styles/index.scss';
---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist';
+
+const devtool = devMode ? undefined : 'source-map';
+
+module.exports = {
+  mode,
+  target,
+  devtool,
+  context: path.resolve(__dirname, 'src'),
+  entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'assets/scripts/[name].[contenthash].js',
+    publicPath: '/',
+    clean: true,
+    assetModuleFilename: 'assets/images/[hash][ext]',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      hash: true,
+      template: path.resolve(__dirname, 'src', 'template.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/[name].[contenthash].css',
+    }),
+  ],
+};
